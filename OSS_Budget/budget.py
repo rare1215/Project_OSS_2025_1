fede047f6e2d9eba56c8fbf0e3b8f125fd958742
JiHyun Sung <hyun051512@gmail.com>
2025-06-10 00:30:16 +0900

    첫번째 커밋: 카테고리 선택를 선택해 해당 카테고리의 지출 출력

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -19,6 +19,39 @@ class Budget:
         for idx, e in enumerate(self.expenses, 1):
             print(f"{idx}. {e}")
         print()
+        
+    def category_expenses(self, category):
+
+         #등록된 카테고리 목록 출력
+        categories = set(e.category for e in self.expenses)
+        if not categories:
+            print("등록된 카테고리가 없습니다.\n")
+            return
+
+        print("\n[카테고리 목록]")
+        for idx, category in enumerate(categories, 1):
+            print(f"{idx}. {category}")
+        print()
+
+        #사용자로부터 카테고리 입력받기
+        category_choice_idx = int(input("선택 >")) - 1
+        if category_choice_idx < 0 or category_choice_idx >= len(categories):
+            print("존재하지 않는 카테고리입니다다.\n")
+            return
+
+        #카테고리 불러오기
+        selected_category = list(categories)[category_choice_idx]
+
+        #선택된 카테고리의 지출 내역 출력
+        category_expenses = [e for e in self.expenses if e.category == selected_category]
+        
+        if not category_expenses:
+            print(f"{selected_category} 카테고리의 지출 내역이 없습니다.\n")
+            return
+        print(f"\n[{selected_category} 카테고리의 지출 목록]")
+        for idx, e in enumerate(category_expenses, 1):
+            print(f"{idx}. {e}")
+        print()
 
     def total_spent(self):
         total = sum(e.amount for e in self.expenses)
